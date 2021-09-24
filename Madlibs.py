170a20d93519e902fe867d43dfe0c17a9154e969
fozanthony <89929155+fozanthony@users.noreply.github.com>
2021-09-24 18:47:27 -0400

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,24 @@
+
+adv = input("Adverb: ")
+verb1 = input("Verb: ")
+conj1 = input("Conjunction: ")
+noun1 = input("Noun: ")
+noun2 = input("Noun: ")
+noun3 = input("Noun: ")
+verb2 = input("Verb: ")
+verb3 = input("Verb: ")
+conj2 =input("Conjunction: ")
+noun4 = input("Noun: ")
+pronoun1 = input("Pronoun: ")
+conj3 = input("Conjunction: ")
+pronoun2 = input("Pronoun: ")
+verb4 = input("Verb: ")
+pronoun3 = input("Pronoun: ")
+conj4 = input("Conjunction: ")
+
+Madlib = f"The weird this is, the day started {adv} great. I recall that i {verb1} up early, for some reason, {conj1} couldnt go back to sleep. So i got out of {noun1} \
+    tiptoed to the {noun2}, peed, and did my usual slow-motion {noun3} walk to get down our squeaky stairs without {verb2} up the rents or Jeffrey. I {verb3} in the kitchen \
+    to suck down some OJ, {conj2} then continued my silent journey to the basement. My {noun4} has a separate little office down there. {pronoun1} is an accountant, {conj3} \
+    because {pronoun2} sometimes {verb4} really late hours during tax season, {pronoun3} had the walls filled with extra insulation for warmth {conj4} soundproofing."
+
+print(Madlib)
